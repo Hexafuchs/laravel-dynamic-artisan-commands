7b33560b4c14734834bb953cd91e986ab61fb84d
Leo <leo@hexafuchs.de>
2024-06-30 14:20:32 +0200

    :white_check_mark: Add stacktrace to tests

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -1,5 +1,5 @@
 <?php
 
 arch('it will not use debugging functions')
-    ->expect(['dd', 'dump', 'ray'])
+    ->expect(['dd', 'dump', 'ray', 'debug_print_backtrace', 'debug_backtrace', 'die'])
     ->each->not->toBeUsed();
