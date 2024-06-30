b31424fa2c163ccecc537a0b5f48e92847180fb0
Leo <leo@hexafuchs.de>
2024-06-30 14:15:38 +0200

    :technologist: Setup developer workbench

---
+++ workbench/bootstrap/providers.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    \Hexafuchs\DynamicArtisanServiceProvider\DynamicConsoleSupportServiceProvider::class,
+    \Hexafuchs\DynamicArtisanServiceProvider\DynamicArtisanServiceProvider::class,
+    \Hexafuchs\DynamicArtisanServiceProvider\PackageServiceProvider::class,
+];
